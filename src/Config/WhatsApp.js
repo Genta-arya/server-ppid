7f020eb2d06f51189bb853b8bf7be8d3d86e76a6
GentaArya <mgentaarya@yahoo.com>
2026-03-03 12:28:01 +0700

    updates url

--- src/Config/WhatsApp.js
+++ src/Config/WhatsApp.js
@@ -12,8 +12,10 @@ export const sendWhatsapp = async (to, message) => {
         headers: {
           Authorization: process.env.FONNTE_TOKEN,
         },
-      }
+      },
     );
+    
+    console.log("WhatsApp sent successfully");
   } catch (error) {
     console.log("WA Error:", error.message);
   }
