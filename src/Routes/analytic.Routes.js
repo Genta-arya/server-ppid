e54cd893816cfde8d8fb5f49a132f5bf79a56482
GentaArya <mgentaarya@yahoo.com>
2026-04-03 20:55:03 +0700

    add security

--- src/Routes/analytic.Routes.js
+++ src/Routes/analytic.Routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { GetAIInsightStatus, GetAnalytics, PostAnalytic } from "../Controller/analytic.controller.js";
+import { GetAIInsightStatus, GetAllDataAnalytic, GetAnalytics, PostAnalytic } from "../Controller/analytic.controller.js";
 
 const routerAnalytic = Router();
 
 routerAnalytic.post("/analytic", PostAnalytic);
 routerAnalytic.get("/analytic", GetAnalytics);
+routerAnalytic.get("/analytic/all", GetAllDataAnalytic);
 routerAnalytic.get("/insight/status", GetAIInsightStatus);
 
 export default routerAnalytic;
