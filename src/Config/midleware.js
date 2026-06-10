f13a7f8ed102228ce030a438a039210e468112cf
GentaArya <mgentaarya@yahoo.com>
2026-06-10 09:39:22 +0700

    midleware update

--- src/Config/midleware.js
+++ src/Config/midleware.js
@@ -1,14 +1,21 @@
-// Middleware untuk cek API Key
 const apiKeyMiddleware = (req, res, next) => {
-  // 1. Loloskan request OPTIONS (Preflight) tanpa cek API Key
+  // 1. Loloskan request OPTIONS (Preflight)
   if (req.method === "OPTIONS") {
     return next();
   }
 
+  // 2. DAFTAR RUTE YANG DIIZINKAN TANPA API KEY
+  const bypassRoutes = ["/api/notifications", "/api/form/file/download"];
+  
+  // Jika path saat ini ada di dalam daftar bypass, lewati validasi
+  if (bypassRoutes.some((route) => req.originalUrl.includes(route))) {
+    return next();
+  }
+
+  // 3. Validasi Key untuk rute lainnya
   const clientKey = req.headers["x-api-key"];
   const secretKey = process.env.X_API_KEY;
 
-  // 2. Validasi Key
   if (!clientKey || clientKey !== secretKey) {
     return res.status(401).json({
       status: "error",
@@ -16,7 +23,7 @@ const apiKeyMiddleware = (req, res, next) => {
     });
   }
 
-  next(); // 3. Lanjut ke webRoutes
+  next();
 };
 
 export default apiKeyMiddleware;
